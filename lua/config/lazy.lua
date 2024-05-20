e835e0cfabf1c1f41577be3b234e78a4763f6962
Sebastian Zęderowski <sebek.x@gmail.com>
2024-05-20 11:11:40 +0200

    Update lazy.lua

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -29,6 +29,9 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.yaml" },
 
     { import = "lazyvim.plugins.extras.coding.codeium" },
+
+    { import = "lazyvim.plugins.extras.dap.core" },
+    { import = "lazyvim.plugins.extras.dap.nlua" },
     -- import/override with your plugins
     { import = "plugins" },
   },
