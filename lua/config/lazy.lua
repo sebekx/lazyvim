382aa8e8b0ec3fdd4acd568f62ecf5903e3c366d
Sebastian Zęderowski <sebek.x@gmail.com>
2024-05-21 09:30:22 +0200

    Update lazy.lua

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -20,7 +20,9 @@ require("lazy").setup({
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
-    -- { import = "lazyvim.plugins.extras.lang.clangd" },
+    { import = "lazyvim.plugins.extras.lang.clangd" },
+    { import = "lazyvim.plugins.extras.lang.docker" },
+    { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.cmake" },  
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.python" },
